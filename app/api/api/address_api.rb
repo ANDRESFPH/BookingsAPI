fcab0623f83d9affb5473d4fbe4faf80873acd4e
Andres <andresfph1989@gmail.com>
2016-06-29 12:01:25 -0500

    API for address
    completed, but without testing

---
+++ app/api/api/address_api.rb
@@ -0,0 +1,100 @@
+class API::AddressApi < Grape::API
+
+  #Api Format, configures the format to json
+  format :json
+  default_format :json
+
+
+  resource :addresses do
+
+  	desc 'Return all the Addresses'
+  	get do
+  	  result = Addr::GetAddresses.call
+      error!(result.error, :not_found) if result.failure?
+      present result.addresses, with: Entities::Address
+  	end
+
+
+  	desc 'Return an Address'
+  	params do
+  	  requires :id, 
+                type: Integer, 
+                desc: 'Address id',
+                values: (1..1_000_000)
+  	end
+  	route_param :id do
+  	  get do
+  	    result = Addr::GetAddress.call(address_id: params[:id])
+  	    error!(result.error, :not_found) if result.failure?
+        Entities::Address.represent(result.address)
+  	  end
+  	end 
+
+
+  	desc 'Create an Address'
+  	params do
+  		requires :street, 
+  				      type: String,
+  				      desc: 'street name'
+  		requires :city,
+  				      type: String,
+  				      desc: 'name of the city'
+  		requires :country,
+  				      type: String,
+  				      desc: 'country name' 
+      optional :zip,
+                type: String,
+                desc: 'zip code'
+  	end
+  	post do
+  	  result = Addr::CreateAddress.call(street: params[:street],
+                                        city: params[:city],
+                                        country: params[:country],
+                                        zip: params[:zip] )
+  	  error!(result.error, :unprocessable_entity) if result.failure? 
+  	end
+
+
+    desc 'update an Address'
+    params do
+      requires :street, 
+                type: String,
+                desc: 'street name'
+      requires :city,
+                type: String,
+                desc: 'name of the city'
+      requires :country,
+                type: String,
+                desc: 'country name' 
+      optional :zip,
+                type: String,
+                desc: 'zip code'
+    end
+    route_param :id do
+      put do
+        result = Addr::UpdateAddress.call(street: params[:street],
+                                          city: params[:city],
+                                          country: params[:country],
+                                          zip: params[:zip] )
+        error!(result.error, :unprocessable_entity) if result.failure? 
+      end
+    end
+    
+
+    desc 'Delete an Address'
+    params do
+      requires :id,
+                type: Integer, 
+                desc: 'Address ID.',
+                values: (1..1_000_000)
+    end
+    route_param :id do
+      delete do
+        result = Addr::DeleteAddress.call(address_id: params[:id])
+        error!(result.error, :unprocessable_entity) if result.failure? 
+      end
+    end
+    
+    
+  end
+end
